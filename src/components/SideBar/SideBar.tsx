175f03688b5cd90e7c359e78ac0f616626a34e14
Robert Coe <chakrawc@gmail.com>
2020-11-19 19:59:58 -0800

    kind of working

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -7,7 +7,7 @@ import { Nav } from "../Nav/Nav";
 export const SideBar = (): JSX.Element => (
 	<div className="side-bar">
 		<header>
-			<img src="/assets/img/logo.png" height={28} />
+			<img src="/wcd/assets/img/logo.png" height={28} />
 		</header>
 		<Nav />
 	</div>
